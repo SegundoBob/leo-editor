e95ea249f3531a3586665ef73c2b2aad07042759
Edward K. Ream <edreamleo@gmail.com>
2025-08-19 05:56:02 -0500

    Update all pylint tests so they pass

--- leo/unittests/plugins/test_importers.py
+++ leo/unittests/plugins/test_importers.py
@@ -3426,7 +3426,7 @@ class TestPython(BaseTestImporter):
 
         def start(self):
             if self.force or ask_yes_no(
-                "Really delete all ipython history? ", default="no", interrupt="no"
+                "Really delete all ipython history?", default="no", interrupt="no"
             ):
                 HistoryTrim.start(self)
 
@@ -3460,6 +3460,8 @@ class TestPython(BaseTestImporter):
                     'class HistoryTrim(BaseIPythonApplication):\n'
                     '\n'  # Leo 6.8.7
                     '    @others\n'
+                    '\n'  # Leo 6.8.7
+                    '\n'  # Leo 6.8.7
             ),
             (2, 'HistoryTrim.start',
                     'description = trim_hist_help\n'
@@ -3474,21 +3476,21 @@ class TestPython(BaseTestImporter):
                     '                    PRIMARY KEY (session, line))""")\n'
                     '    new_db.commit()\n'
                     '    new_hist_file.rename(hist_file)\n'
-                    '\n'  # Leo 6.8.7
             ),
             (1, 'class HistoryClear',
                     'class HistoryClear(HistoryTrim):\n'
                     '\n'  # Leo 6.8.7
                     '    @others\n'
+                    '\n'  # Leo 6.8.7
+                    '\n'  # Leo 6.8.7
             ),
             (2, 'HistoryClear.start',
 
                     'def start(self):\n'
                     '    if self.force or ask_yes_no(\n'
-                    '        "Really delete all ipython history? ", default="no", interrupt="no"\n'
+                    '        "Really delete all ipython history?", default="no", interrupt="no"\n'
                     '    ):\n'
                     '        HistoryTrim.start(self)\n'
-                    '\n'  # Leo 6.8.7
             ),
             (1, 'class HistoryApp',
                     'class HistoryApp(Application):\n'
@@ -3514,7 +3516,7 @@ class TestPython(BaseTestImporter):
                     '        return self.subapp.start()\n'
             ),
         )
-        self.new_run_test(s, expected_results, trace=True)
+        self.new_run_test(s, expected_results, trace=False)
     #@+node:ekr.20230612072414.1: *3* TestPython.test_long_declaration
     def test_long_declaration(self):
 
@@ -3652,6 +3654,7 @@ class TestPython(BaseTestImporter):
                     'class C1:\n'
                     '    """Class docstring"""\n'
                     '    @others\n'
+                    '\n'
             ),
             (2, 'C1.__init__',
                     'def __init__(self):\n'
@@ -3844,6 +3847,7 @@ class TestPython(BaseTestImporter):
                     'class C1:\n'
                     '    """Class docstring"""\n'
                     '    @others\n'
+                    '\n'
             ),
             (2, 'C1.__init__',
                     'def __init__(self):\n'
@@ -3856,111 +3860,6 @@ class TestPython(BaseTestImporter):
             ),
         )
         self.new_run_test(s, expected_results)
-    #@+node:ekr.20250818150351.1: *3* TestPython.test_pylint_fail
-    def test_pylint_fail(self) -> None:
-
-        s = '''
-                # Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-                # For details: https://github.com/pylint-dev/pylint/blob/main/LICENSE
-                # Copyright (c) https://github.com/pylint-dev/pylint/blob/main/CONTRIBUTORS.txt
-
-                """Checker for anything related to the async protocol (PEP 492)."""
-
-                from __future__ import annotations
-
-                import sys
-                from typing import TYPE_CHECKING
-
-                import astroid
-                from astroid import nodes, util
-
-                from pylint import checkers
-                from pylint.checkers import utils as checker_utils
-                from pylint.checkers.utils import decorated_with
-
-                if TYPE_CHECKING:
-                    from pylint.lint import PyLinter
-
-
-                class AsyncChecker(checkers.BaseChecker):
-                    name = "async"
-                    msgs = {
-                        "E1700": (
-                            "Yield inside async function",
-                            "yield-inside-async-function",
-                            "Used when an `yield` or `yield from` statement is "
-                            "found inside an async function.",
-                            {"minversion": (3, 5)},
-                        ),
-                        "E1701": (
-                            "Async context manager '%s' doesn't implement __aenter__ and __aexit__.",
-                            "not-async-context-manager",
-                            "Used when an async context manager is used with an object "
-                            "that does not implement the async context management protocol.",
-                            {"minversion": (3, 5)},
-                        ),
-                    }
-
-                    def open(self) -> None:
-                        self._mixin_class_rgx = self.linter.config.mixin_class_rgx
-                        self._async_generators = ["contextlib.asynccontextmanager"]
-
-                    @checker_utils.only_required_for_messages("yield-inside-async-function")
-                    def visit_asyncfunctiondef(self, node: nodes.AsyncFunctionDef) -> None:
-                        for child in node.nodes_of_class(nodes.Yield):
-                            if child.scope() is node and (
-                                sys.version_info[:2] == (3, 5) or isinstance(child, nodes.YieldFrom)
-                            ):
-                                self.add_message("yield-inside-async-function", node=child)
-
-                    @checker_utils.only_required_for_messages("not-async-context-manager")
-                    def visit_asyncwith(self, node: nodes.AsyncWith) -> None:
-                        for ctx_mgr, _ in node.items:
-                            inferred = checker_utils.safe_infer(ctx_mgr)
-                            if inferred is None or isinstance(inferred, util.UninferableBase):
-                                continue
-
-                            if isinstance(inferred, nodes.AsyncFunctionDef):
-                                # Check if we are dealing with a function decorated
-                                # with contextlib.asynccontextmanager.
-                                if decorated_with(inferred, self._async_generators):
-                                    continue
-                            elif isinstance(inferred, astroid.bases.AsyncGenerator):
-                                # Check if we are dealing with a function decorated
-                                # with contextlib.asynccontextmanager.
-                                if decorated_with(inferred.parent, self._async_generators):
-                                    continue
-                            else:
-                                try:
-                                    inferred.getattr("__aenter__")
-                                    inferred.getattr("__aexit__")
-                                except astroid.exceptions.NotFoundError:
-                                    if isinstance(inferred, astroid.Instance):
-                                        # If we do not know the bases of this class,
-                                        # just skip it.
-                                        if not checker_utils.has_known_bases(inferred):
-                                            continue
-                                        # Ignore mixin classes if they match the rgx option.
-                                        if (
-                                            "not-async-context-manager"
-                                            in self.linter.config.ignored_checks_for_mixins
-                                            and self._mixin_class_rgx.match(inferred.name)
-                                        ):
-                                            continue
-                                else:
-                                    continue
-                            self.add_message(
-                                "not-async-context-manager", node=node, args=(inferred.name,)
-                            )
-
-
-                def register(linter: PyLinter) -> None:
-                    linter.register_checker(AsyncChecker(linter))
-        '''
-        expected_results = (
-
-        )
-        self.new_run_test(s, expected_results, check=False, trace=True)
     #@+node:ekr.20250813102014.1: *3* TestPython.test_python_reference_test
     def test_python_reference_test(self):
 
@@ -4005,7 +3904,9 @@ class TestPython(BaseTestImporter):
             (1, 'class MyClass',
                     'class MyClass:\n'
                     '    """MyClass: docstring"""\n'
-                    '    @others\n'  ### .replace('AT', '@')
+                    '    @others\n'
+                    '\n'
+                    '\n'
             ),
             (2, 'MyClass.f1',
                     'def f1(self):\n'
@@ -4122,6 +4023,7 @@ class TestPython(BaseTestImporter):
             (1, 'class Class3',
                 'class Class3:\n'
                 '@others\n'
+                '\n'  # Leo 6.8.7
             ),
             (2, 'Class3.u1',
                     '# Outer underindented comment\n'
