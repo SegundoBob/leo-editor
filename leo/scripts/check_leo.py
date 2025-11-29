96c541aec0f517d992c28e8976d3ee0bcb83ecbb
Edward K. Ream <edreamleo@gmail.com>
2025-11-29 06:07:45 -0600

    Add check_leo.py script

---
+++ leo/scripts/check_leo.py
@@ -0,0 +1,11 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20251129060454.1: * @file ../scripts/check_leo.py
+#@@language python
+
+"""
+Check Leo's core files for attribute errors not found by mypy, pylint or
+pyflakes.
+
+This script should run about as fast as pyflakes_leo.py.
+"""
+#@-leo
