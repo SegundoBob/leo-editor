39ad8eeef0e0eeda1e1464b85613017d3416ef24
Edward K. Ream <edreamleo@gmail.com>
2026-01-08 19:27:50 -0600

    Update beautify_all_leo script

--- leo/scripts/beautify_all_leo.py
+++ leo/scripts/beautify_all_leo.py
@@ -5,8 +5,6 @@
 """
 beautify_all_leo.py: Beautify (almost) all of Leo's files.
 
-This script should work regardless of whether mypyc has compiled leoTokens.py!
-
 Info item #3867 describes all of Leo's test scripts:
 https://github.com/leo-editor/leo-editor/issues/2867
 
@@ -25,22 +23,23 @@ print(os.path.basename(__file__))
 leo_editor_dir = os.path.abspath(os.path.join(__file__, '..', '..', '..'))
 os.chdir(leo_editor_dir)
 
-# Beautify all, and always issue a report.
-args = '--all --beautified --write --report'
+args = f"--config {leo_editor_dir}{os.sep}pyproject.toml"
 isWindows = sys.platform.startswith('win')
 python = 'py' if isWindows else 'python'
 
 # Use -m so that __name__ == '__main__'.
+command_head = f"{python} -m ruff format {args}"
+
 for command in [
-    # f'{python} -m "leo.core.leoTokens" {args} leo/commands',
-    # f'{python} -m "leo.core.leoTokens" {args} leo/core',
-    # f'{python} -m "leo.core.leoTokens" {args} leo/external',
-    # f'{python} -m "leo.core.leoTokens" {args} leo/plugins',
-    # f'{python} -m "leo.core.leoTokens" {args} leo/scripts',
-    # f'{python} -m "leo.core.leoTokens" {args} leo/modes',
-    # f'{python} -m "leo.core.leoTokens" {args} leo/unittests/commands',
-    # f'{python} -m "leo.core.leoTokens" {args} leo/unittests/plugins',
-    # f'{python} -m "leo.core.leoTokens" {args} leo/unittests/misc_tests',
+    # f"{command_head} leo{os.sep}commands",
+    f"{command_head} leo{os.sep}core",
+    # f"{command_head} leo{os.sep}external",
+    ## f"{command_head} leo{os.sep}plugins",
+    ## f"{command_head} leo{os.sep}scripts",
+    # f"{command_head} leo{os.sep}modes",
+    # f"{command_head} leo{os.sep}unittests{os.sep}commands",
+    # f"{command_head} leo{os.sep}unittests{os.sep}plugins",
+    # f"{command_head} leo{os.sep}unittests{os.sep}misc_tests",
 ]:
     subprocess.Popen(command, shell=True).communicate()
 # @-leo
