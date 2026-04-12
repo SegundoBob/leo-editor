bda22e004e1687b5cbec3b22f4d0be14fb991e9f
Edward K. Ream <edreamleo@gmail.com>
2026-04-12 05:24:14 -0500

    Improve detection of classes

--- leo/plugins/importers/java.py
+++ leo/plugins/importers/java.py
@@ -19,12 +19,26 @@ class Java_Importer(Importer):
 
     language = 'java'
 
+    # @+<< Java_Importer: block_patterns >>
+    # @+node:ekr.20260412045240.1: *3* << Java_Importer: block_patterns >>
     block_patterns: tuple = (
+        # protected Autotest(InstallerCommandLine commandLine) throws IOException, DriverException {
+        
         ('class',     re.compile(r'^.*?\bclass\s+(\w+)')),
-        ('public',    re.compile(r'^\s*public\s+(\w+.*?)\(.*?\)\s*{')),
-        ('private',   re.compile(r'^\s*private\s+(\w+.*?)\(.*?\)\s*{')),
         ('interface', re.compile(r'^\s*interface\s+(\w*)\s*{')),
+        
+        # private/protected/public classes...
+        ('private',   re.compile(r'^\s*private\s+(\w+.*?)\(.*?\)\s*((throws|implements).*?)?{')),
+        ('protected', re.compile(r'^\s*protected\s+(\w+.*?)\(.*?\)\s*((throws|implements).*?)?{')),
+        ('public',    re.compile(r'^\s*public\s+(\w+.*?)\(.*?\)\s*(t(throws|implements).*?)?{')),
+        
+        # private/protected/public functions...
+        ('private',   re.compile(r'^\s*private\s+(\w+.*?)\(.*?\)\s*{')),
+        ('protected', re.compile(r'^\s*protected\s+(\w+.*?)\(.*?\)\s*{')),
+        ('public',    re.compile(r'^\s*public\s+(\w+.*?)\(.*?\)\s*{')),
     )  # fmt: skip
+
+    # @-<< Java_Importer: block_patterns >>
 
 
 # @-others
