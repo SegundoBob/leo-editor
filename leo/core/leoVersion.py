b918154080ac4a179fa131013e38381d22d596cc
Edward K. Ream <edreamleo@gmail.com>
2026-04-14 05:56:28 -0500

    Update version

--- leo/core/leoVersion.py
+++ leo/core/leoVersion.py
@@ -64,7 +64,7 @@ leoVersion.version:     Leo's version number.
 # 6.8.7:  October 11, 2025.
 # 6.8.8:  April 14, 2026
 # @-<< version dates >>
-version = '6.8.8'
+version = '6.8.9-devel'
 static_date = 'April 14, 2026'
 # @@language python
 # @@tabwidth -4
