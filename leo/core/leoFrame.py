ce364319751797768e8d306cde28f7a14cfd5dc4
Edward K. Ream <edreamleo@gmail.com>
2026-04-23 08:41:36 -0500

    Disable trace

--- leo/core/leoFrame.py
+++ leo/core/leoFrame.py
@@ -498,8 +498,7 @@ class LeoFrame:
     def copyText(self, event: LeoKeyEvent = None) -> None:
         """Copy the selected text from the widget to the clipboard."""
         w = event.widget if event else None
-        if w and not g.unitTesting:  ###
-            g.trace(g.isTextWrapper(w), w.__class__.__name__, repr(w.name))
+        # g.trace(g.isTextWrapper(w), w.__class__.__name__, repr(w.name))
         if not w or not g.isTextWrapper(w):
             return
         # Set the clipboard text.
