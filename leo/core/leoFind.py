625ebb016a04ddd7a68696f2e436ccbd3db3dd22
Edward K. Ream <edreamleo@gmail.com>
2026-05-29 00:24:14 -0500

    Reformat

--- leo/core/leoFind.py
+++ leo/core/leoFind.py
@@ -2186,7 +2186,7 @@ class LeoFind:
             if equal(settings, bunch):
                 return
 
-        # Insert the setting at the current place in the list. 
+        # Insert the setting at the current place in the list.
         self.prev_searches_i += 1
         self.prev_searches.insert(self.prev_searches_i, settings)
 
